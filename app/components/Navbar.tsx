46daf0038220e8d4c970b101b29f7758af691d2f
Daksh Purohit <dakshgrows@gmail.com>
2026-01-31 01:32:08 +0530

    landing page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,10 +3,7 @@ import { motion } from "framer-motion"
 import { ArrowRight } from "lucide-react"
 import Link from "next/link"
 
-const links = [
-    { name: "Features", href: "/features" },
-    { name: "Pricing", href: "/pricing" },
-]
+
 
 
 export default function Navbar() {
@@ -14,12 +11,7 @@ export default function Navbar() {
         <>
             <section className="flex flex-row text-[#808080] p-4 px-10 justify-between">
                 <div className="font-bold text-2xl">OpsOs</div>
-                <div className="flex gap-5 cursor-pointer">
-                    {links.map((link) => (
-                        <Link className="mx-2 text-gray-400 transition-colors hover:text-white hover:underline underline-offset-4 decoration-2" key={link.name} href={link.href}>{link.name}</Link>
-                    ))}
-                </div>
-                <button className="text-slate-50 border-2 rounded-md px-5 cursor-pointer">
+                <button className="text-slate-50 border-2 rounded-md px-5 py-2 cursor-pointer">
                     Log In
                 </button>
             </section>
